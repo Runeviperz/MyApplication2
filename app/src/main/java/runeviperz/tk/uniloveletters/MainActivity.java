95c17004da9b26659d128275a513ae694b6278ea
Runeviperz <battlefield200@gmail.com>
2015-07-28 23:25:27 +1000

    added hello world

--- app/src/main/java/runeviperz/tk/uniloveletters/MainActivity.java
+++ app/src/main/java/runeviperz/tk/uniloveletters/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -12,7 +13,10 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        TextView lol = new TextView(this);
+        lol.setText("Hello world");
+        lol.setTextSize(40);
+        setContentView(lol);
     }
 
     @Override
