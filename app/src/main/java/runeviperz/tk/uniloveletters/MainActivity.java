a74e437a9c32d0b116685540901c515a59671dbc
Runeviperz <battlefield200@gmail.com>
2015-07-29 17:35:01 +1000

    - main screen layout test

--- app/src/main/java/runeviperz/tk/uniloveletters/MainActivity.java
+++ app/src/main/java/runeviperz/tk/uniloveletters/MainActivity.java
@@ -13,31 +13,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        TextView lol = new TextView(this);
-        lol.setText("Hello world");
-        lol.setTextSize(420);
-        setContentView(lol);
+        setContentView(R.layout.activity_main);
+
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
